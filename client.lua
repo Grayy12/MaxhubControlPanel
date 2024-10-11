34fe28093e047209a16e0ba2ad49852e43b78930
Grayy12 <grayy161@gmail.com>
2024-10-11 19:18:26 -0400

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,4 +1,6 @@
-return -- shutdown
+if true then 
+    return
+end -- shutdown 
 --local queuetp = queue_on_teleport or queueonteleport or function(v) end
 
 --queuetp('loadstring(game:HttpGet("https://raw.githubusercontent.com/Grayy12/MaxhubControlPanel/main/client.lua",true))()')
